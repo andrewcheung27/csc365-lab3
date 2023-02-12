8f1cedefb09ed417b64881d01f4cf59223b9adf3
andrewcheung27 <andrewcheung27@gmail.com>
2023-02-12 12:58:39 -0800

    completed

--- part 2/lab submission/AIRLINES/AIRLINES-modify.py
+++ part 2/lab submission/AIRLINES/AIRLINES-modify.py
@@ -1,7 +1,38 @@
 # Author: Andrew Cheung
 # Email: acheun29@calpoly.edu
+
+
 import mysql.connector
 from mysql.connector import Error
+
+
+def execute_sql(cursor):
+    # remove all flights except the ones to/from AKI
+    cursor.execute("DELETE FROM Flights WHERE !(SourceAirport = 'AKI') AND !(DestAirport = 'AKI');")
+
+    # for flights not operated by Continental, Virgin, or AirTran, increase the
+    # flight number by 2000.
+    # airline numbers: Continental = 7, Virgin = 12, AirTran = 10
+    cursor.execute("UPDATE Flights SET FlightNo = FlightNo + 2000 WHERE Airline = 7 OR Airline = 12 OR Airline = 10;")
+
+    # for pairs of flights to/from AKI that aren't operated by Continental,
+    # FlightNo += 1 if even, FlightNo -= 1 if odd.
+    # do even += 2001, odd -= 1, even -= 2000 so there are no duplicates
+    cursor.execute("UPDATE Flights SET FlightNo = FlightNo + 2001 WHERE Airline = 7 AND MOD(FlightNo, 2) = 0;")
+    cursor.execute("UPDATE Flights SET FlightNo = FlightNo - 1 WHERE Airline = 7 AND MOD(FlightNo, 2) = 1;")
+    cursor.execute("UPDATE Flights SET FlightNo = FlightNo - 2000 WHERE Airline = 7 AND MOD(FlightNo, 2) = 0;")
+
+    # set airline to Continental for all flights to/from AKI,
+    # except Virgin and AirTran
+    cursor.execute("UPDATE Flights SET Airline = 7 WHERE Airline != 12 AND Airline != 10;")
+
+    # output contents
+    cursor.execute("SELECT * FROM Flights ORDER BY Airline, FlightNo;")
+    records = cursor.fetchall()
+    print("------------------------------------------------------------------")
+    for r in records:
+        print(r)
+    print("------------------------------------------------------------------")
 
 
 def main():
@@ -25,12 +56,7 @@ def main():
                                             database=dbName, user=userName,
                                             password=passString)
         cursor = connector.cursor()
-        cursor.execute("SELECT * FROM Airlines")
-        records = cursor.fetchall()
-        print("----------------------------------------------------")
-        for r in records:
-            print(r)
-        print("----------------------------------------------------")
+        execute_sql(cursor)
 
     except Error as e:
         print("connection error:", e)
